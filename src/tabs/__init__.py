a24ef5bb169392974136ad71c30e91578b88ae1e
Zachariah Moore <zpaynemoore@gmail.com>
2025-08-08 13:33:34 -0400

    Updated for new tabs

--- src/tabs/__init__.py
+++ src/tabs/__init__.py
@@ -1,16 +1,21 @@
-# MTM Character Sheet Tabs Package
-# This package contains all the individual tab modules for the character sheet application
+"""MTM Character Sheet Tabs Package
+This package exposes the tab classes used by the application.
+"""
 
 from .basic_info_tab import BasicInfoTab
 from .aspects_tab import AspectsTab
 from .gear_die_tab import GearDieTab
 from .inventory_tab import InventoryTab
 from .abilities_tab import AbilitiesTab
+from .encyclopedia_tab import EncyclopediaTab
+from .dice_roller_tab import DiceRollerTab
 
 __all__ = [
     'BasicInfoTab',
-    'AspectsTab', 
+    'AspectsTab',
     'GearDieTab',
     'InventoryTab',
-    'AbilitiesTab'
-] 
+    'AbilitiesTab',
+    'EncyclopediaTab',
+    'DiceRollerTab',
+]
